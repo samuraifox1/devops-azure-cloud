bb2507c5b66851af29c584553276c12cf8a52c2b
samuraifox1 <86284310+samuraifox1@users.noreply.github.com>
2021-10-27 12:21:37 +0530

    Added lines to sql file

--- DBScripts/db-script01.sql
+++ DBScripts/db-script01.sql
@@ -1,1 +1,4 @@
 --- This SQL file is used by the Pipeline for DB Deployment
+INSERT INTO Persons VALUES (2, 'Rajesh', 'Swarnkar', 'Mumbai', 'Thane');
+INSERT INTO Persons VALUES (3, 'Rakesh', 'SS', 'Kalyan', 'Thane');
+INSERT INTO Persons VALUES (4, 'Kum', 'SS', 'Hari Sadan', 'Mumbai');
